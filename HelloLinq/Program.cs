9b1c92ff4aff00b5989e9a43c7a84944a1fbd0b4
kszicsillag <kszicsillag@gmail.com>
2020-03-04 19:25:01 +0100

    első feladat

--- HelloLinq/Program.cs
+++ HelloLinq/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace HelloLinq
 {
@@ -6,9 +7,37 @@ namespace HelloLinq
     {
         public static IEnumerable<Dog> Dogs => Dog.Repository.Values;
 
+        private static List<Dog> ListDogsByNamePrefix(IEnumerable<Dog> dogs,
+            string prefix)
+        {
+            var result = new List<Dog>();
+            foreach (var dog in dogs)
+            {
+                if (dog.Name.StartsWith(prefix,
+                    StringComparison.OrdinalIgnoreCase))
+                    result.Add(dog);
+            }
+            return result;
+        }
+
+        private static string GetInput()
+        {
+            Console.Write("Írd be a kutya nevének elejét: ");
+            var result = Console.ReadLine();
+            Console.Clear();
+            return result;
+        }
+
+
         static void Main(string[] args)
         {
-            
+            string searchText;
+            while ((searchText = GetInput()).Length > 0)
+            {
+                foreach (var dog in ListDogsByNamePrefix(Dogs, searchText))
+                    Console.WriteLine(dog);
+            }
+
         }
     }
 }
