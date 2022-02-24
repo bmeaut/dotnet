c2104f1debfb1c8dea75a7f0cb2794785d28bd18
Simon Gábor <simon.gabor@autsoft.hu>
2022-02-24 10:34:32 +0100

    upgrade net6

--- HelloLinq/Program.cs
+++ HelloLinq/Program.cs
@@ -1,43 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
+using HelloLinq;
 
-namespace HelloLinq
+static List<Dog> ListDogsByNamePrefix(IEnumerable<Dog> dogs,
+    string prefix)
 {
-    class Program
+    var result = new List<Dog>();
+    foreach (var dog in dogs)
     {
-        public static IEnumerable<Dog> Dogs => Dog.Repository.Values;
-
-        private static List<Dog> ListDogsByNamePrefix(IEnumerable<Dog> dogs,
-            string prefix)
-        {
-            var result = new List<Dog>();
-            foreach (var dog in dogs)
-            {
-                if (dog.Name.StartsWith(prefix,
-                    StringComparison.OrdinalIgnoreCase))
-                    result.Add(dog);
-            }
-            return result;
-        }
-
-        private static string GetInput()
-        {
-            Console.Write("Írd be a kutya nevének elejét: ");
-            var result = Console.ReadLine();
-            Console.Clear();
-            return result;
-        }
-
-
-        static void Main(string[] args)
-        {
-            string searchText;
-            while ((searchText = GetInput()).Length > 0)
-            {
-                foreach (var dog in ListDogsByNamePrefix(Dogs, searchText))
-                    Console.WriteLine(dog);
-            }
-
-        }
+        if (dog.Name.StartsWith(prefix,
+            StringComparison.OrdinalIgnoreCase))
+            result.Add(dog);
     }
+    return result;
 }
+
+static string GetInput()
+{
+    Console.Write("Írd be a kutya nevének elejét: ");
+    var result = Console.ReadLine();
+    Console.Clear();
+    return result;
+}
+
+string searchText;
+IEnumerable<Dog> Dogs = Dog.Repository.Values;
+while ((searchText = GetInput()).Length > 0)
+{
+    foreach (var dog in ListDogsByNamePrefix(Dogs, searchText))
+        Console.WriteLine(dog);
+}
+
+
